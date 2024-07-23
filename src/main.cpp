c6c06c9068eda37c7b822afa71978ce3ce6349af
TuteMthCD <matias.civadda2342001@gmail.com>
2024-07-23 13:51:03 -0300

    added: potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "FreeRTOS.h"
+#include "hardware/adc.h"
 #include "hardware/i2c.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdio.h"
@@ -12,6 +13,9 @@
 #define SDA 0
 #define SCL 1
 
+#define NUM_PWM 16
+
+
 void i2c_task(void*) {
     i2c_init(i2c0, 400000);
     gpio_set_function(SDA, GPIO_FUNC_I2C);
@@ -24,40 +28,62 @@ void i2c_task(void*) {
 
         0x00, // mode 1
         0x00, // mode 2
-        0xB2, // pwm 0
-        0xB2, // pwm 1
-        0xB2, // pwm 2
-        0xB2, // pwm 3
-        0xB2, // pwm 4
-        0xB2, // pwm 5
-        0xB2, // pwm 6
-        0xB2, // pwm 7
-        0xB2, // pwm 8
-        0xB2, // pwm 9
-        0xB2, // pwm 10
-        0xB2, // pwm 11
-        0xB2, // pwm 12
-        0xB2, // pwm 13
-        0xB2, // pwm 14
-        0xB2, // pwm 15
+        //
+        0x00, // pwm 0
+        0x00, // pwm 1
+        0x00, // pwm 2
+        0x00, // pwm 3
+        0x00, // pwm 4
+        0x00, // pwm 5
+        0x00, // pwm 6
+        0x00, // pwm 7
+        0x00, // pwm 8
+        0x00, // pwm 9
+        0x00, // pwm 10
+        0x00, // pwm 11
+        0x00, // pwm 12
+        0x00, // pwm 13
+        0x00, // pwm 14
+        0x00, // pwm 15
+        //
         0x00, // GRPPWM
         0x00, // GRPFREQ
+        //
         0xAA, // LEDOUT 0
         0xAA, // LEDOUT 1
         0xAA, // LEDOUT 2
         0xAA, // LEDOUT 3
+        //
         0x00, // SUBADDR1
         0x00, // SUBADDR2
         0x00, // SUBADDR3
         0x00, // ALLCALLADDR
+        //
         0xFF, // IREF
         0x00, // EFLAG 1
         0x00  // EFLAG 2
     };
 
     i2c_write_blocking(i2c0, 0x60, buff.data(), buff.size(), false);
+
+    adc_init();
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(0);
+
     while(true) {
-        vTaskDelay(1000);
+
+        uint8_t pwm_value = adc_read() >> 4;
+
+        std::vector<uint8_t> set_pwm = { 0x02 | 0x80 }; // select reg 0x02, auto-increment
+        for(int i = 0; i < NUM_PWM; i++) {
+            set_pwm.push_back(pwm_value);
+        }
+        i2c_write_blocking(i2c0, 0x60, set_pwm.data(), set_pwm.size(), false);
+
+
+        vTaskDelay(10);
     }
 }
 
