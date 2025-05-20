8b4c96bc2d47ac6bac87c824f64d76148b11ece0
TuteMthCD <matias.civadda2342001@gmail.com>
2025-05-20 10:34:40 -0300

    added: clang format

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,28 @@
 #include "task.h"
 #include <stdio.h>
 
-void led_task(void *) {
-  bool pin = 0;
-  while (true) {
-    pin = !pin;
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, pin);
-    vTaskDelay(1000);
-  }
+void led_task(void*)
+{
+	bool pin = 0;
+	while (true) {
+		pin = !pin;
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, pin);
+		vTaskDelay(1000);
+	}
 }
 
-int main() {
-  stdio_init_all();
+int main()
+{
+	stdio_init_all();
 
-  if (cyw43_arch_init()) {
-    printf("cyw43 error on init");
-    return -1;
-  } else {
-    printf("cyw43 init correctly");
-  }
+	if (cyw43_arch_init()) {
+		printf("cyw43 error on init");
+		return -1;
+	} else {
+		printf("cyw43 init correctly");
+	}
 
-  xTaskCreate(&led_task, "LED_Task", 256, NULL, 1, NULL);
+	xTaskCreate(&led_task, "LED_Task", 256, NULL, 1, NULL);
 
-  vTaskStartScheduler();
+	vTaskStartScheduler();
 }
